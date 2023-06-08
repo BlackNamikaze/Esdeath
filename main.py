7f53dc90eb862cdda014e223abc3ea77dcfa54e3
BlackNamikaze <135922097+BlackNamikaze@users.noreply.github.com>
2023-06-07 21:26:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# our main file.
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
